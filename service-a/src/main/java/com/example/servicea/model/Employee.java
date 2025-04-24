35b10779da3ccca767006e0a92008420400a0cf4
Jisha Joshi <joshijisha070@gmail.com>
2025-04-24 14:46:16 +0530

    added getter setter

--- service-a/src/main/java/com/example/servicea/model/Employee.java
+++ service-a/src/main/java/com/example/servicea/model/Employee.java
@@ -5,9 +5,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 @Entity
 public class Employee {
+    // Getters and Setters
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -23,13 +28,4 @@ public class Employee {
     public Employee() {
     }
 
-    // Getters and Setters
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
-    public String getDepartment() { return department; }
-    public void setDepartment(String department) { this.department = department; }
 }
